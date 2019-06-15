b6cd0cb68ccc305d4e42db45ff8c91fba130b131
Birkan Pala <birkanpala@gmail.com>
2019-06-15 12:55:37 +0300

    Initial commit

---
+++ src/main/java/com/evbox/everon/EveronApplication.java
@@ -0,0 +1,13 @@
+package com.evbox.everon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveronApplication.class, args);
+	}
+
+}
